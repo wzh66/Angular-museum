edf3498e025c232d1054b9af6ac2e0ed7c093ea9
wzh66 <1179030633@qq.com>
2020-05-22 15:15:28 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ResearchComponent } from './pages/research/research.component';
+
+import { NewsDetailsComponent } from './pages/news-details/news-details.component';
+import {IndexComponent} from './pages/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    ContactComponent,
+    ResearchComponent,
+    NewsDetailsComponent
   ],
   imports: [
     BrowserModule

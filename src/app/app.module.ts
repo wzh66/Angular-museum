bb88b22ff7ac01f3e3078336c836dfb23297c691
wzh66 <wzh514500>
2020-05-22 15:12:52 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
+import {ThemeModule} from './@theme/theme.module';
+
+import {INTERCEPTORS} from './@core/interceptors';
+
+
+import {AppComponent} from './app.component';
+
+import {environment} from '../environments/environment';
+import {IndexComponent} from './pages/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [INTERCEPTORS, {provide: 'PREFIX_URL', useValue: environment.prefix_url}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
